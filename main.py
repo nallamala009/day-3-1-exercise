f70f52f24828bcdb76664d5e44959c3a75de69f9
nallamala009 <nallamalasrijwala@gmail.com>
2021-03-20 05:56:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+if number % 2 == 0:
+  print("This is a even number")
+else:
+  print("This is odd number")
+#Write your code below this line 👇
+
+
+
+
